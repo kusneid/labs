b27c32e425048a0743e2042fb7afb4b5ff1d5029
Владислав Байдаков <kusneid@gmail.com>
2023-12-02 16:46:37 +0300

    laba 7 done

--- laba7/laba7/main.cpp
+++ laba7/laba7/main.cpp
@@ -1,8 +1,9 @@
 #include "h.h"
-//32 10 9
+
 int main() {
 	system("chcp 1251 > nul");
 	setlocale(LC_ALL, "Russian");
+	
 	//cout<<static_cast<int>('\n');
 	ifstream key("key.txt");
 	if (!key){return 0;}
@@ -10,7 +11,7 @@ int main() {
 	int stat[256][256]= {0};
 	
 	//keylen
-	char s[128];
+	char s[300];
 	int l = 1,i = 0,temp_key=0;
 	for (key >> s; !key.eof(); key >> s) { l++; }
 	//keymas
@@ -18,6 +19,7 @@ int main() {
 	int* ke = new int[l];
 	key.clear();
 	key.seekg(0);
+	
 	//bool newl = true;
 	while (key.get(ch)){
 		unsigned char t = static_cast<unsigned char>(ch);
@@ -52,8 +54,9 @@ int main() {
 		encoded<<temp;
 		i++;
 	}*/
-	char* exc = new char[sizeof(source)/sizeof(char)];
-	int* exk = new int[1000];
+	char* exc = new char[1000];
+	int* exk = new int[500];
+	
 	int y = 0,x;
 	for (source.get(ch),i=0;!source.eof();source.get(ch),i++){
 		//cout<<static_cast<int>(ch)<<' '<<ke[i%l]%256<<'\n';
@@ -99,7 +102,8 @@ int main() {
 	ch = 'y';
 	char temp;
 	int g,count;
-	while (ch == 'y') {
+	
+	/*while (ch == 'y') {
 		count=0;
 		cout << "Введите символ для анализа: ";
 		cin >> temp;
@@ -116,16 +120,19 @@ int main() {
 		cout << "\nСимвол был закодирован " << count << " раз\nПродолжить?(y/n)";
 		cin >> ch;
 		
-	}
+	}*/
 	
-
+	bool exis;
 	ofstream st("stat.txt");
-	st<<0<<' ';
+
+	st << setw(2) << 0 << setw(4);
 	for (int n=0;n<256;n++){
 		st<<n<<' ';
 	}
+	st << "\n";
 	for(int v=0;v<256;v++){
-		st<<(char)v<<' ';
+		if ((char)v == '»' || (char)v == 'ї' || (char)v == 'п') { continue; }
+		exis = false;
 		for (int b=0;b<256;b++){
 			/*if ((stat[v][b]!=0)&&(stat[v][b]!=10)&&(stat[v][b]!=32)&&(stat[v][b]!=9)){
 				st<<(char)stat[v][b]<<' ';
@@ -134,10 +141,47 @@ int main() {
 			{
 				st<<stat[v][b]<<' ';
 			}*/
-			st<<stat[v][b]<<' ';
+			
+			if (stat[v][b] != 0) {
+				exis = true;
+			}
 		}
-		st<<"\n";
+		if (exis == true){ 
+			st << v << ' ';
+			for (int bg = 0; bg < 256; bg++) {
+				st << stat[v][bg] << ' ';
+			 }
+			st << "\n";
+		}
+		
 	}
+	
+	
+
+
+	/*int check[256] = { 0 };
+	int len2 = 0;
+	for (int i2 = 0; i2 < 256; i2++) {
+		len2 = 0;
+		for (int j2 = 0; j2 < 256; j2++) {
+			if (stat[i2][j2] != 0) { len2++; }
+
+		}
+		if (len2 != 0) { check[i2] = 1; }
+
+	}
+	for (int i3 = 0; i3 < 256; i3++) {
+		if (check[i3] == 1) {
+			st << char(i3) << ' ';
+			for (int j3 = 0; j3 < 256; j3++) {
+				st << stat[i3][j3] << ' ';
+			}
+
+		}
+		st << "\n";
+		
+	}*/
+
 	
 	return 0;
 }
