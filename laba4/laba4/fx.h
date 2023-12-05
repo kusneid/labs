9aa25357d8133299ffadeb56ccab10e02e149c7a
Владислав Байдаков <kusneid@gmail.com>
2023-12-05 17:09:48 +0300

    main

--- laba4/laba4/fx.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-#include <iomanip>
-using namespace std;
-void iter(double x,double eps,int k);
-
-void dele(double x, double eps, int k);
-
-void newt(double x, double eps, int k);
