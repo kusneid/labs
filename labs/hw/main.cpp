d1beed0dd2a04ad3f72bc5d535b55a1276fa2140
kusneid <144150704+kusneid@users.noreply.github.com>
2023-12-14 09:43:40 +0300

    xd hw

--- labs/hw/main.cpp
+++ labs/hw/main.cpp
@@ -23,26 +23,26 @@ int main() {
 		i++;
 
 	} while (mas[i-1]!='.');
-
+i--;
 	for (int y = 0; y < i; y++) {
 		std::cout << mas[y]<<"";
 	}
 	std::cout << "\n";
 	
 	int Index_start = 0;
-	for (int i1 = i-2; i1 > -1; i1--) {
+	for (int i1 = i; i1 > -1; i1--) {
 		//std::cout << mas[i1] << "";
 		if (mas[i1] == ' ') {
-			Index_start = i1;
+			Index_start = i1+1;
 			break;
-		}
+		}//не работает при одном слове 
 	}
 	/*if (Index_start == -1) {
 		std::cout << "Error indexstart";
 	}*/
 	char* word = new char[i-Index_start];
 	/*std::cout << i - Index_start-2;*/
-	for (int i1 = Index_start+1; i1 < i-1; i1++)
+	for (int i1 = Index_start; i1 <= i; i1++)
 	{
 		
 		word[i1 - Index_start] = mas[i1];
@@ -55,16 +55,16 @@ int main() {
 	temp = '[';
 	char* wordi= new char[31];
 	int cur_len = 0, i2 = 0;
-	for (size_t i = 0; mas[i] != '.'; i++)
+	for (size_t i7 = 0; mas[i7] != '.'; i7++)
 	{
-		if (mas[i] != ' ')
+		if (mas[i7] != ' ')
 		{
 			wordi[i2] = mas[i];
 			i2++;
 		}
 		else
 		{
-			if (strcmp(wordi, word) == 0)
+			if (strcmp(wordi, word) != 0)
 			{
 				PrintWord(wordi, i2 + 1 - 1);
 			}
