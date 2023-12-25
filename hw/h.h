20906a64d1a5d39efcd1c6575151ba758b2527f2
Владислав Байдаков <kusneid@gmail.com>
2023-12-25 22:31:13 +0300

    all

---
+++ hw/h.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <conio.h>
+#include <cstring>
+bool Compare(char *word, char *word_temp, int lastWordLen, int wordCurrentLen);
+void Fill(char *mas, int &i);
+char *LastWord(int i, char *mas, int &len);
+void PrintWord(char *word, int len);
+void Print(char *word, int word_len, char *mas, int mas_len);
